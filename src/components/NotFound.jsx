a401d54f4bcae1561fad174a10f02d96d7d14b9b
GiteshGayakwad <143732488+GiteshGayakwad@users.noreply.github.com>
2024-07-14 10:27:20 +0530

    admin dashboad

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="bg-[#0e262b] h-screen w-screen flex flex-col justify-center items-center text-3xl text-white">
-      <Link to={"/admin"}>
+      <Link to={"/admin/products"}>
         <button className="flex flex-col items-center justify-center">
           <img src="./Images/notFound.png" alt="" className="w-[40%] " />
         </button>
